382af1c149a4b56803950f57e3efb82ddf84d6ad
ismael molina lugo <ismaelmolina209@gmail.com>
2019-08-31 00:13:06 -0600

    update

---
+++ js/any.js
@@ -0,0 +1,6 @@
+var x = 4;
+console.log(x);
+x = "hola mundo";
+console.log(x);
+x = false;
+console.log(x);
---
+++ ts/any.ts
@@ -0,0 +1,6 @@
+let x: any = 4;
+console.log(x);
+x = "hola mundo";
+console.log(x);
+x = false;
+console.log(x);
