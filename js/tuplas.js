e8420b90d04c64ae24be892ae44a4466430ff629
ismael molina lugo <ismaelmolina209@gmail.com>
2019-08-30 23:42:15 -0600

    update

---
+++ js/tuplas.js
@@ -0,0 +1,4 @@
+var x;
+x = ["hola", 1];
+console.log(x[0].substr(2));
+console.log(x[1].toString());
---
+++ ts/tuplas.ts
@@ -0,0 +1,4 @@
+let x: [string,number];
+x = ["hola",1];
+console.log(x[0].substr(2));
+console.log(x[1].toString());
