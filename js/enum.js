6e5b80553c2669c5cee05bd0acad627697b21442
ismael molina lugo <ismaelmolina209@gmail.com>
2019-08-31 00:04:52 -0600

    update

--- js/enum.js
+++ js/enum.js
@@ -1,11 +1,11 @@
 //sin asignar valor (solo se le asigna uno)
 var color;
 (function (color) {
-    color[color["rojo"] = 0] = "rojo";
-    color[color["verde"] = 1] = "verde";
-    color[color["negro"] = 2] = "negro";
-    color[color["azul"] = 3] = "azul";
-    color[color["blanco"] = 4] = "blanco";
+    color[color["rojo"] = 1] = "rojo";
+    color[color["verde"] = 2] = "verde";
+    color[color["negro"] = 3] = "negro";
+    color[color["azul"] = 4] = "azul";
+    color[color["blanco"] = 5] = "blanco";
 })(color || (color = {}));
-var c = color.azul;
-console.log(c);
+var nomcolor = color[4];
+console.log(nomcolor);
--- ts/enum.ts
+++ ts/enum.ts
@@ -1,4 +1,4 @@
 //sin asignar valor (solo se le asigna uno)
-enum color {rojo,verde,negro,azul,blanco}
-var c: color = color.azul;
-console.log(c);
+enum color {rojo=1,verde,negro,azul,blanco}
+var nomcolor: string = color[4];
+console.log(nomcolor);
