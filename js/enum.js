39068472c6bc1a30829aab59c1b531c5a87c1ceb
ismael molina lugo <ismaelmolina209@gmail.com>
2019-08-30 23:58:19 -0600

    update

--- js/enum.js
+++ js/enum.js
@@ -1,9 +1,11 @@
-var direccion;
-(function (direccion) {
-    direccion[direccion["arriba"] = 1] = "arriba";
-    direccion[direccion["abajo"] = "abajp".length] = "abajo";
-    direccion[direccion["izq"] = 2] = "izq";
-    direccion[direccion["der"] = 4] = "der";
-})(direccion || (direccion = {}));
-var a = direccion.arriba;
-console.log(a);
+//sin asignar valor (solo se le asigna uno)
+var color;
+(function (color) {
+    color[color["rojo"] = 0] = "rojo";
+    color[color["verde"] = 1] = "verde";
+    color[color["negro"] = 2] = "negro";
+    color[color["azul"] = 3] = "azul";
+    color[color["blanco"] = 4] = "blanco";
+})(color || (color = {}));
+var c = color.azul;
+console.log(c);
