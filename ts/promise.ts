ffc4ec478cac09636508300b5ed62ac4223ecfc4
ismael molina lugo <ismaelmolina209@gmail.com>
2019-09-01 20:18:43 -0600

    update

--- ts/promise.ts
+++ ts/promise.ts
@@ -1,13 +1,25 @@
-//cadena de promesas
-Promise.reject(new Error("Algo malo sucedio"))
-    .then((res)=>{
-        console.log(res);
-        return 456;
-    })
-    .then((res)=>{
-        console.log(res);
-    })
-    .catch((err)=>{
-        console.log(err.message);
-        return Promise.resolve(123);
+//promesas en paralelo
+function loadItem(id: number): Promise<{id: number}> {
+    return new Promise((resolve)=>{
+        console.log("cargando item", id);
+        setTimeout(()=>{
+            resolve({id: id});
+        },1000);
     });
+}
+//cadena (serie)
+let item1,item2;
+loadItem(1)
+    .then((res)=>{
+        item1 = res;
+        return loadItem(2);
+    })
+    .then((res)=>{
+        item2 = res;
+        console.log("hecho");
+    });
+    Promise.all([loadItem(1),loadItem(2)])
+        .then((res)=>{
+            [item1,item2]=res;
+            console.log("hecho");
+        });
